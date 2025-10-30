938a34c792529053e70c1e7895b4fe8a71ce4beb
crocusia <rainbowsubin0522@gmail.com>
2025-10-31 03:34:26 +0900

    [Feat] 주문, 주문 아이템 컨트롤러 및 DTO 추가

---
+++ src/main/java/com/example/ecommerce/dto/order/OrderItemStatus.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.dto.order;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "주문 상품 상태")
+public enum OrderItemStatus {
+    @Schema(description = "주문됨")
+    ORDERED,
+
+    @Schema(description = "준비중")
+    PREPARING,
+
+    @Schema(description = "배송중")
+    SHIPPED,
+
+    @Schema(description = "배송완료")
+    DELIVERED,
+
+    @Schema(description = "취소")
+    CANCELLED,
+
+    @Schema(description = "반품")
+    RETURNED,
+
+    @Schema(description = "교환")
+    EXCHANGED
+}
