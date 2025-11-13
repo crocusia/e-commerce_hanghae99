a824b9784a6be308e0d69154289c98d30b917368
crocusia <rainbowsubin0522@gmail.com>
2025-11-13 23:19:51 +0900

    [Feat] 결제 레포지토리 추가

---
+++ src/main/java/com/example/ecommerce/payment/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce.payment.repository;
+
+import com.example.ecommerce.payment.domain.Payment;
+import java.util.Optional;
+
+public interface PaymentRepository {
+    Payment save(Payment payment);
+
+    Optional<Payment> findById(Long id);
+
+    Payment findByIdOrElseThrow(Long id);
+}
